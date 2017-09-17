c035335c0a53e19e23027458c20192e13730bc67
sqz777 <sqz777isme@gmail.com>
2017-09-17 12:11:31 +0800

    init

---
+++ Codewars_Exes and Ohs/Codewars_Exes and Ohs/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_Exes_and_Ohs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
