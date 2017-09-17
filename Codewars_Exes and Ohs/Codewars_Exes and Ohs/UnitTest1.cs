3fd2d4b22e9ad8d0473d3f1d7708bfc9f4720adb
sqz777 <sqz777isme@gmail.com>
2017-09-17 12:14:31 +0800

    first test case failed

--- Codewars_Exes and Ohs/Codewars_Exes and Ohs/UnitTest1.cs
+++ Codewars_Exes and Ohs/Codewars_Exes and Ohs/UnitTest1.cs
@@ -7,8 +7,21 @@ namespace Codewars_Exes_and_Ohs
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_Null_Should_Be_true()
         {
+            //arrange
+            var input = string.Empty;
+            //act
+            var actual = Kata.XO(input);
+            //assert
+        }
+    }
+
+    public class Kata
+    {
+        public static bool XO(string input)
+        {
+            throw new NotImplementedException();
         }
     }
 }
